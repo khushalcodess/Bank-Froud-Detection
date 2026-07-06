5c7b1bc3bf7761a0bf8465140fc5fa1830f85584
Khushal Patil <khushalpatil3055@gmail.com>
2026-07-07 00:20:27 +0530

    Fix API URL to use Render backend

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
 const API = axios.create({
- baseURL: 'http://localhost:3001/api',
-
+  baseURL: process.env.REACT_APP_API_URL || 'https://fraudguard-backend-9ss1.onrender.com/api',
 });
 
-API.interceptors.request.use((req)=>{
-    const token = localStorage.getItem('token');
-    if (token) {
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
-
 });
 
 export default API;
