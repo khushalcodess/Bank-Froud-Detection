99c830a5dd368c55412afe22bf16da8493fb0505
Khushal Patil <khushalpatil3055@gmail.com>
2026-07-06 12:39:18 +0530

    Add ML models for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const mongooseURI = "mongodb://localhost:27017/froud-detection-system"
-const connecttomongo = async ()=>{
-    try {
-        await mongoose.connect(mongooseURI);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-        process.exit(1);
-    }
+
+const connecttomongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB Atlas ✅");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 }
+
 module.exports = connecttomongo;
